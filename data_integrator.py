e620eaa35520f3e100aa1ce4363adceef98f0190
zhangyp <541159401@qq.com>
2024-07-04 17:46:28 +0800

    1、初步完成目标库为DM的业务批量插入

--- data_integrator.py
+++ data_integrator.py
@@ -44,7 +44,7 @@ class DataIntegrator:
                                    self.dst_db_config.get("user"),
                                    self.dst_db_config.get("password"),
                                    self.dst_db_config.get("dbname"))
-        else:# TODO 替换成工厂模式
+        else:  # TODO 替换成工厂模式
             db_instance = DM(self.dst_db_config.get("user"),
                              self.dst_db_config.get("password"),
                              self.dst_db_config.get("host"),
@@ -53,28 +53,44 @@ class DataIntegrator:
         # 解析表字段
         cols = db_instance.get_table_structure(self.dst_db_schema, db_table)
 
-        # 生成模拟数据
+        # 生成单条数据函数
+        def cols_data_generator():
+            cols_data = []  # 列数据（字段值）
+            fields_data = []
+            for col in cols:
+                gen_mode = self.data_src_config.get("mockup").get("model")
+                if gen_mode == "simple":
+                    if col[3].lower() in ("n", "no"):  # 只添加不为空的字段
+                        col_data = MockData().data_generator_simple(col[1], col[2])
+                        # 若生成的数据不为空，则添加到列数据中
+                        if col_data != "":
+                            cols_data.append(col_data)
+                            fields_data.append(col[0])
+                else:
+                    col_data = MockData().data_generator_business(col[0], col[1], col[2], col[3])
+                    # 若生成的数据不为空，则添加到列数据中
+                    if col_data != "":
+                        cols_data.append(col_data)
+                        fields_data.append(col[0])
+            return cols_data, fields_data
+
+        # 生成指定数量
         rows_data = []  # 行数据 （每一条数据表记录）
         row_amount = self.task_config.get("DataSize")
         logger.info(f"1、表{db_table}正在生成{row_amount}条数据...")
         for row in range(row_amount):
             logger.debug(f"正在生成第{row}条数据...")
-            cols_data = []  # 列数据（字段值）
-            for col in cols:
-                gen_mode = self.data_src_config.get("mockup").get("model")
-                if gen_mode == "simple":
-                    if col[3].lower() in ("n", "no"):  # 只添加不为空的字段
-                        cols_data.append(MockData().data_generator_simple(col[1], col[2]))
-                else:
-                    # TODO 逻辑需要完善，根据字段名、类型生成
-                    cols_data.append(MockData().data_generator_business(col[1], col[2]))
-            logger.debug(f"第{row}条数据：{cols_data}")
-            rows_data.append(cols_data)
+            gen_col_data = cols_data_generator()[0]
+            logger.debug(f"第{row}条数据：{gen_col_data}")
+            rows_data.append(gen_col_data)
         logger.info(f"生成数据成功，共生成{len(rows_data)}条数据")
 
         # 拼接sql语句
         logger.info(f"2、正在拼接SQL语句...")
-        fields = [col[0] for col in cols if col[3].lower() in ("n", "no")]
+        # fields = [col[0] for col in cols if col[3].lower() in ("n", "no")] # 简单模式用
+        # 根据行数据中值不为空的字段名拼成字段列
+        fields = cols_data_generator()[1]
+
         fields_str = ",".join(fields)
         placeholder = ",".join(["?" for _ in range(len(fields))])
         sql_statement = "INSERT INTO " + self.dst_db_schema + "." + db_table + f" ({fields_str}) " + " VALUES " + f"({placeholder})"
@@ -92,7 +108,7 @@ class DataIntegrator:
         if self.task_config.get("IfMultiThread"):
             threads = []
             for table in self.dst_db_tables:
-                threads.append(threading.Thread(target=self.data_gen_insert, args=(table,)))
+                threads.append(threading.Thread(target=self.thread_function, args=(table,)))
             # 启动线程
             [t.start() for t in threads]
 
@@ -102,9 +118,12 @@ class DataIntegrator:
                 logger.info(f"任务开始执行...")
                 self.data_gen_insert(table)
 
+    # 线程id打印函数
+    def thread_function(self, table_name):
+        logger.info(f"表{table_name}数据开始插入-线程ID:{threading.get_ident()}")
+        self.data_gen_insert(table_name)
 
 
 if __name__ == "__main__":
-    # DataIntegrator().data_gen_insert("dm", "IMCIS", "UserMst")
     di = DataIntegrator()
     di.task_run()
--- mock_data.py
+++ mock_data.py
@@ -10,8 +10,11 @@ import uuid
 
 import mimesis
 from mimesis import Locale
+from mimesis import Person
 
 from config import GLOBAL_CONFIG
+
+from log_config import logger
 
 
 # 生成数据
@@ -21,6 +24,9 @@ class MockData:
         self.config = GLOBAL_CONFIG.get("DataSource")
         self.varchar_max_length = self.config.get("mockup").get("varchar_max_length")
         self.int_max = self.config.get("mockup").get("int_max")
+        self.decimal_max = self.config.get("mockup").get("decimal_max")
+        self.age_range = self.config.get("mockup").get("age")
+        self.year_range = self.config.get("mockup").get("year")
 
     # varchar生成器
     def generate_varchar(self, column_length):
@@ -35,43 +41,139 @@ class MockData:
     def generate_int(self):
         return random.randint(0, self.int_max)
 
+    # 增加业务逻辑的高级生成器，可以根据实际目标数据库的需求，进行扩展
+    def generate_varchar_advanced(self, column_name, column_length, column_is_nullable):
+        mimesis_gen = mimesis.Generic(Locale.ZH)
+        mimesis_person = Person(Locale.ZH)
+        if column_name.lower() == "sex":
+            return random.choice(["男", "女"])
+        elif column_name.lower == "ageunit":
+            return "岁"
+        elif column_name.lower().endswith("name"):
+            if column_name.lower().startswith("organization"):
+                return "哈尔滨市第二医院"
+            elif column_name.lower().startswith("requestorg"):
+                return "申请机构"
+            elif column_name.lower().startswith("requestdept"):
+                return "申请科室"
+            else:
+                # return mimesis_gen.person.full_name()
+                return mimesis_gen.person.last_name() + mimesis_gen.person.first_name()
+        elif column_name.lower().endswith("uid"):
+            if column_name.lower().startswith(("service", "media", "resultservice", "study")):
+                return "00000000-0000-0000-0000-000000000000"
+            else:
+                return str(uuid.uuid4()).upper()
+        elif column_name.lower() in ["patientmasterid", "businessid", "visitid"]:
+            return str(uuid.uuid4())
+        elif column_name.lower() == "patientclass":
+            return random.choice(["门诊", "住院", "急诊", "体检"])
+        elif column_name.lower() == "servicesectid":
+            return random.choice(["CT", "MR", "DR", "CR", "XA", "MG", "RF", "US"])
+        elif column_name.lower() == "organizationid":
+            return "QWYHZYFAZX"
+        elif column_name.lower() == "account":
+            return mimesis_gen.person.username(mask="l")
+        elif column_name.lower() in ["patientid", "accessionnumber", "medrecno"]:
+            return random.randint(0, self.int_max)
+        elif column_name.lower() in ("birthday", "birthdate", "birth"):
+            return mimesis_person.birthdate(min_year=1970, max_year=2023)
+        elif column_name.lower() == "resultstatuscode":
+            return random.choice(["3050", "3080", "3090"])
+        elif column_name.lower() in ["businesstype", "classcode"]:
+            return "Exam"
+        elif column_name.lower() == "typecode":
+            return "ExamFilm"
+        # 如果匹配不到以上规则字段，根据该字段是否可为空，空则返回空字符串
+        elif column_is_nullable.lower() in ("no", "n"):
+            if column_length >= self.varchar_max_length:
+                column_length = self.varchar_max_length
+            return "".join(random.choice(string.ascii_letters + string.digits) for _ in range(int(column_length)))
+        else:
+            return ""
+
+    def generate_bit_advanced(self, column_name):
+        if column_name.lower().endswith("flag"):
+            return 0
+        elif column_name.lower() == "age":
+            return random.randint(self.age_range[0], self.age_range[1])
+        else:
+            return random.choice([0, 1])
+
+    def generate_int_advanced(self, column_name):
+        # 标记、状态类型
+        if column_name.lower().endswith(("flag", "status", "state", "need", "count")):
+            return random.choice([0, 1])
+        elif column_name.lower() in ["patientid", "accessionnumber", "medrecno"]:
+            return random.randint(0, self.int_max)
+        else:
+            return 0
+
+    def generate_decimal_advanced(self, column_type):
+        if column_type.lower() in ("decimal", 'numeric', "float", "double"):
+            return random.randint(0, self.decimal_max)
+        else:
+            return 0.0
+
+    def generate_time_advanced(self, column_type):
+        mimesis_gen = mimesis.Generic(Locale.ZH)
+        if column_type.lower() in ["datetime", "timestamp"]:
+            return mimesis_gen.datetime.datetime(start=self.year_range[0], end=self.year_range[1]).strftime(
+                "%Y-%m-%d %H:%M:%S")
+        elif column_type.lower() == "date":
+            return mimesis_gen.datetime.date(start=self.year_range[0], end=self.year_range[1]).strftime("%Y-%m-%d")
+        elif column_type.lower() == "time":
+            return mimesis_gen.datetime.time().strftime("%H:%M:%S")
+        else:
+            return ""
+
     # 数据生成器
     def data_generator_simple(self, column_type, column_length):
         mimesis_gen = mimesis.Generic(Locale.ZH)
         data_generators = {
             "varchar": lambda: self.generate_varchar(column_length),
-            # "varchar": lambda: self.generate_varchar(4),
             "bit": lambda: random.choice([0, 1]),
             "int": lambda: self.generate_int(),
             "integer": lambda: self.generate_int(),
             "smallint": lambda: self.generate_int(),
-            "tinyint": lambda: random.choice(0, 1),
-            "float": lambda: random.uniform(0, self.int_max),
-            "double": lambda: random.uniform(0, self.int_max),
-            "decimal": lambda: random.uniform(0, self.int_max),
-            "numeric": lambda: random.uniform(0, self.int_max),
+            "tinyint": lambda: random.choice([0, 1]),
+            "numeric": lambda: self.generate_decimal_advanced(column_type),
+            "decimal": lambda: self.generate_decimal_advanced(column_type),
+            "double": lambda: self.generate_decimal_advanced(column_type),
+            "float": lambda: self.generate_decimal_advanced(column_type),
             "date": lambda: mimesis_gen.datetime.date().strftime("%Y-%m-%d"),
             "datetime": lambda: mimesis_gen.datetime.datetime().strftime("%Y-%m-%d %H:%M:%S"),
             "time": lambda: mimesis_gen.datetime.time().strftime("%H:%M:%S"),
             "timestamp": lambda: mimesis_gen.datetime.datetime().strftime("%Y-%m-%d %H:%M:%S")
         }
-
         # 获取数据生成函数
         data_gen_func = data_generators.get(column_type.lower())
-
         return data_gen_func()
 
     # 业务模式数据生成
-    def data_generator_business(self, column_type, column_length):
-        mimesis_gen = mimesis.Generic(Locale.ZH)
+    def data_generator_business(self, column_name, column_type, column_length, column_is_nullable):
         data_generators = {
-            "varchar": lambda: self.generate_varchar(column_length),
-            "bit": lambda: random.choice([0, 1]),
-            "int": lambda: self.generate_int(),
-            "integer": lambda: self.generate_int(),
-            "smallint": lambda: self.generate_int(),
+            "varchar": lambda: self.generate_varchar_advanced(column_name, column_length, column_is_nullable),
+            "bit": lambda: self.generate_bit_advanced(column_name),
+            "int": lambda: self.generate_int_advanced(column_name),
+            "integer": lambda: self.generate_int_advanced(column_name),
+            "smallint": lambda: self.generate_int_advanced(column_name),
+            "numeric": lambda: self.generate_decimal_advanced(column_type),
+            "decimal": lambda: self.generate_decimal_advanced(column_type),
+            "double": lambda: self.generate_decimal_advanced(column_type),
+            "float": lambda: self.generate_decimal_advanced(column_type),
+            "date": lambda: self.generate_time_advanced("date"),
+            "datetime": lambda: self.generate_time_advanced("datetime"),
+            "time": lambda: self.generate_time_advanced("time"),
+            "timestamp": lambda: self.generate_time_advanced("timestamp")
         }
-        # TODO 完善业务模式数据生成
+
+        data_gen_func = data_generators.get(column_type.lower())
+        if data_gen_func:
+            return data_gen_func()
+        else:
+            logger.warn(f"未找到类型为{column_type.lower()}的生成函数，返回空字符")
+        return ""
 
 
 # 从数据库中获取数据
